83ff962ca3025bd8f63343475e9db57aace6a2ac
Pedro Botega <botega123@hotmail.com>
2025-06-27 16:49:41 -0300

    Corrige server.js

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 const app = require('./src/app');
 
-const tutorRoutes = require('./routes/tutores');
-app.use('/api/tutores', tutorRoutes);
 
-const alunosRoute = require('./routes/alunos');
-app.use('/api/alunos', alunosRoute);
-
-// Rota raiz para ver se está funcionando
-app.get('/', (req, res) => {
-  res.send('✅ API TutorMe funcionando na raiz!');
-});
-
-// Inicia o servidor só depois de configurar todas as rotas
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
