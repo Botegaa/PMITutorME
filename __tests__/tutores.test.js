0f448158489d8d790653b8b6c257dd1ad8a4fea0
Pedro Botega <botega123@hotmail.com>
2025-06-27 17:37:34 -0300

    Correcao

--- __tests__/tutores.test.js
+++ __tests__/tutores.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('./src/app.js');
 
 describe('GET /tutores', () => {
   it('deve retornar status 200', async () => {
