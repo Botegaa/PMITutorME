45536a336880264404e4228ff73cfce2cad8992a
Pedro Botega <botega123@hotmail.com>
2025-06-27 19:15:57 -0300

    commit feature

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./src/app');
 
+app.get('/', (req, res) => {
+  res.send('✅ API TutorMe funcionando na branch feature!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
