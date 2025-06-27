f0d975367b4dff2e99df40902f6fa9923c4813e6
Pedro Botega <botega123@hotmail.com>
2025-06-27 16:19:59 -0300

    ECS2 CHAGESt

--- src/app.js
+++ src/app.js
@@ -26,5 +26,18 @@ app.use('/api/tutores', tutorRoutes);
 
 const alunosRoute = require('./routes/alunos');
 app.use('/api/alunos', alunosRoute);
+// 1) Rota raiz para um “hello world” ou status
+app.get('/', (req, res) => {
+  res.send('✅ API TutorMe funcionando na raiz!');
+});
+
+// 2) Rotas da sua API
+app.use('/api/tutores', tutorRoutes);
+
+// 3) Start do servidor escutando em todas interfaces
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 module.exports = app;
