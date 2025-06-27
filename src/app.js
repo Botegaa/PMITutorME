57b10a9323d3bf3c2af89a3b3c0ad38a669e5e3c
Pedro Botega <botega123@hotmail.com>
2025-06-27 17:21:45 -0300

    testes

--- src/app.js
+++ src/app.js
@@ -10,22 +10,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexão com o MongoDB
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB conectado"))
   .catch((err) => console.error("Erro ao conectar no MongoDB:", err));
 
-// ✅ ROTA RAIZ (para testar no navegador)
+
 app.get('/', (req, res) => {
   res.send('✅ API TutorMe funcionando na raiz!');
 });
 
-// ✅ IMPORTAÇÃO E USO DAS ROTAS
+
 const tutorRoutes = require('./routes/tutores');
 app.use('/api/tutores', tutorRoutes);
 
 const alunosRoute = require('./routes/alunos');
 app.use('/api/alunos', alunosRoute);
 
-// Exporta o app para ser usado no server.js
+
 module.exports = app;
