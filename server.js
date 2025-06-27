850c73d80d9b0ce948fa1b97ea638833ece35440
Pedro Botega <botega123@hotmail.com>
2025-06-27 16:22:43 -0300

    ECS2 CHAGEs2t

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
   
 });
+const tutorRoutes = require('./routes/tutores');
+app.use('/api/tutores', tutorRoutes);
+
+const alunosRoute = require('./routes/alunos');
+app.use('/api/alunos', alunosRoute);
+// 1) Rota raiz para um “hello world” ou status
+app.get('/', (req, res) => {
+  res.send('✅ API TutorMe funcionando na raiz!');
+});
+
+// 2) Rotas da sua API
+app.use('/api/tutores', tutorRoutes);
+
+// 3) Start do servidor escutando em todas interfaces
 
 
 module.exports = app;
--- src/app.js
+++ src/app.js
@@ -21,23 +21,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 .catch((err) => console.error("Erro ao conectar no MongoDB:", err));
 
 
-const tutorRoutes = require('./routes/tutores');
-app.use('/api/tutores', tutorRoutes);
 
-const alunosRoute = require('./routes/alunos');
-app.use('/api/alunos', alunosRoute);
-// 1) Rota raiz para um “hello world” ou status
-app.get('/', (req, res) => {
-  res.send('✅ API TutorMe funcionando na raiz!');
-});
-
-// 2) Rotas da sua API
-app.use('/api/tutores', tutorRoutes);
-
-// 3) Start do servidor escutando em todas interfaces
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
 
 module.exports = app;
