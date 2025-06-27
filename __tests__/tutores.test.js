6284e5d14e5dbb8fa7034eb144122b9b0a2a127c
Pedro Botega <botega123@hotmail.com>
2025-06-27 17:48:07 -0300

    Teste correcao

--- __tests__/tutores.test.js
+++ __tests__/tutores.test.js
@@ -1,9 +1,14 @@
+const mongoose = require('mongoose');
 const request = require('supertest');
-const app = require('../src/app'); 
+const app = require('../src/app');
 
 describe('GET /api/tutores', () => {
   it('deve retornar status 200', async () => {
     const res = await request(app).get('/api/tutores');
     expect(res.statusCode).toBe(200);
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
   });
 });
